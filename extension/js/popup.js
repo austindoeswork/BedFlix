b34a9a84b530803616839ab1c0ed385126c3d22f
TheoRice <theo.rice1996@gmail.com>
2017-10-29 18:14:47 -0400

    separates netflix splash screen from netflix watch screen

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -23,7 +23,21 @@ function hideDIV(divID) {
 function checkURL(statusText) {
   var netflixURL = statusText.substring(0, 23);
 
+  var activity = statusText.substring(24, 29);
+
+  // Checks if the current URL is that of Netflix.
+  // If not on Netflix, display an error message
+  // in div "offNetflix". If on Netflix, and not
+  // currently watching a show, display an error
+  // in div "else". If watching a show, display
+  // controls in the div "watch".
   if (netflixURL == "https://www.netflix.com") {
+    if (activity == "watch") {
+      hideDIV('else');
+    }
+    else {
+      hideDIV('watch');
+    }
     hideDIV('offNetflix');
   }
   else {
